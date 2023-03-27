962df4aca7b6ba5b49bc79e03026b1074394b5a3
shahrier <hossainshahrier.shm@gmail.com>
2023-03-28 02:30:02 +0600

    Updated

--- task16.py
+++ task16.py
@@ -1,22 +1,23 @@
-number_list = []
+
+numbers_list = []
 
 for i in range(7):
-    number_input = int(input("Enter a number: "))
-    number_list.append(number_input)
+    num = int(input("Enter a number: "))
+    numbers_list.append(num)
 
-# [7 6 13 19 18 23 25]
+largest = numbers_list[0]
+second_largest = numbers_list[1]
+largest_index = 0
+second_largest_index = 1
+#  [7, 13, 2, 10, 6, -11, 0]
+for i in range( len(numbers_list)):
+    if numbers_list[i] > largest:
+        second_largest = largest
+        second_largest_index = largest_index
+        largest = numbers_list[i]
+        largest_index = i
+    elif numbers_list[i] > second_largest:
+        second_largest = numbers_list[i]
+        second_largest_index = i
 
-largest_number = number_list[0]
-second_largest_number = number_list[0]
-largest_number_index = 0
-
-for number in range(len(number_list)):
-    if number_list[number] > largest_number:
-        second_largest_number = largest_number
-        largest_number = number_list[number]
-        largest_number_index = number
-    elif number_list[number] > second_largest_number and number_list[number] != largest_number:
-        second_largest_number = number_list[number]
-        largest_number_index = number
-
-print("Second largest number is", second_largest_number, "and its index position on the list is", largest_number_index)
+print("Second largest number is", second_largest, "and its index position on the list is", second_largest_index)
